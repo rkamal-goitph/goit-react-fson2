409276cc9469eb031e10b848a1b56bd8db24a389
Teacher Radh <r.kamal@ph.goit.global>
2024-03-07 22:11:19 +0800

    Add final discussion code with notes

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -11,27 +11,56 @@ import css from './Inventory.module.css';
 // 4. Decide the actions that alter each state
 // 5. Define the actions that alter each state
 
+// We lifted the state up to the inventory component instead of the inventory list
+// so that the inventory form can interact with the state in the inventory component
+
+// we then pass down the state to the inventory list component as props
+// so that we can render then in our inventory list component
+
+// PROPS --> data flow direction is parent to child
+// STATES --> data flow maybe lifted from child to parent so that all children have access to the data
+
 export class Inventory extends Component {
   state = {
     items: [],
     searchTerm: '',
   };
 
+  addItem = newItem => {
+    this.setState(prevState => ({
+      items: [...prevState.items, newItem],
+    }));
+  };
+
+  deleteItem = itemId => {
+    this.setState(prevState => ({
+      items: prevState.items.filter(item => item.id !== itemId),
+    }));
+  };
+
+  updateSearchTerm = term => {
+    this.setState({ searchTerm: term });
+  };
+
   render() {
     const { items, searchTerm } = this.state;
 
+    const filteredItems = items.filter(item =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
       <div className={css.app}>
-        <h2>Shoe Inventory Manager</h2>
+        <h2>Stocks Inventory Manager</h2>
         <InventoryForm addItem={this.addItem} />
         <input
           type="text"
           placeholder="Search Inventory"
-          value={this.searchTerm}
+          value={searchTerm}
           onChange={e => this.updateSearchTerm(e.target.value)}
           className={css.searchInput}
         />
-        <InventoryList items={items} deleteItem={this.deleteItem} />
+        <InventoryList items={filteredItems} deleteItem={this.deleteItem} />
       </div>
     );
   }
--- src/components/InventoryForm/InventoryForm.jsx
+++ src/components/InventoryForm/InventoryForm.jsx
@@ -23,17 +23,65 @@ export class InventoryForm extends Component {
   state = {
     name: '',
     quantity: '',
+    isInStock: true, // Checkbox state
+    condition: 'new', // Radio buttons state
+    category: 'footwear', // Select dropdown state
+  };
+
+  // this is a generic onChange event handler function
+  // which handles the mutation of the state
+  // by using dynamic object key
+
+  handleChange = event => {
+    const { name, value, type, checked } = event.target;
+    this.setState({ [name]: type === 'checkbox' ? checked : value });
+  };
+
+  // static onChange input handlers
+
+  handleNameChange = event => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleQuantityChange = event => {
+    this.setState({
+      quantity: event.target.value,
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    const newItem = {
+      id: nanoid(),
+      name: this.state.name,
+      quantity: parseInt(this.state.quantity),
+      isInStock: this.state.isInStock,
+      condition: this.state.condition,
+      category: this.state.category,
+    };
+
+    this.props.addItem(newItem);
+    this.setState({
+      name: '',
+      quantity: '',
+      isInStock: true,
+      condition: 'new',
+      category: 'footwear',
+    });
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} className={css.form}>
+      <form onSubmit={this.handleFormSubmit} className={css.form}>
         <input
           type="text"
           name="name"
           placeholder="Item Name"
           value={this.state.name}
-          onChange={this.handleChange}
+          onChange={this.handleNameChange}
           required
           className={css.input}
         />
@@ -42,10 +90,52 @@ export class InventoryForm extends Component {
           name="quantity"
           placeholder="Stock Quantity"
           value={this.state.quantity}
-          onChange={this.handleChange}
+          onChange={this.handleQuantityChange}
           required
           className={css.input}
         />
+        <label>
+          <span style={{ marginRight: '10px' }}>In Stock:</span>
+          <input
+            type="checkbox"
+            name="isInStock"
+            checked={this.state.isInStock}
+            onChange={this.handleChange}
+          />
+        </label>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}>
+          Condition:
+          <label>
+            <input
+              type="radio"
+              name="condition"
+              value="new"
+              checked={this.state.condition === 'new'}
+              onChange={this.handleChange}
+            />
+            <span style={{ marginLeft: '10px' }}>New</span>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="condition"
+              value="used"
+              checked={this.state.condition === 'used'}
+              onChange={this.handleChange}
+            />
+            <span style={{ marginLeft: '10px' }}>Used</span>
+          </label>
+        </div>
+        <select
+          name="category"
+          value={this.state.category}
+          onChange={this.handleChange}
+          className={css.input}
+        >
+          <option value="footwear">Footwear</option>
+          <option value="clothing">Clothing</option>
+          <option value="accessories">Accessories</option>
+        </select>
         <button type="submit" className={css.addButton}>
           Add Item to Inventory
         </button>
