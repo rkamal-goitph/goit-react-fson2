886557483716298f5f0422f500237956f39e93a3
Teacher Radh <r.kamal@ph.goit.global>
2024-03-11 21:54:20 +0800

    Comment out `Menu` and `OrderCounter` Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,10 +9,10 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Menu title="Shoe Store">
+        {/* <Menu title="Shoe Store">
           <MenuItemList shoes={shoes} />
         </Menu>
-        <OrderCounter shoes={shoes} />
+        <OrderCounter shoes={shoes} /> */}
         <Inventory />
       </div>
     );
