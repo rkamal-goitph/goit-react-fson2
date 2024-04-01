7b6dda8e4b8f095a69c04fccde40f29438f06d8a
Teacher Radh <r.kamal@ph.goit.global>
2024-04-01 22:29:05 +0800

    Install React Router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,3 @@
-import MenuItemList from './MenuItemList/MenuItemList';
-import salads from '../data/salads.json';
-
 export const App = () => {
-  return (
-    <div>
-      <MenuItemList salads={salads} />
-    </div>
-  );
+  return <div></div>;
 };
