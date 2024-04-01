5ace952abf96f91357685415ccb217a2552fcd36
Teacher Radh <r.kamal@ph.goit.global>
2024-04-01 22:38:15 +0800

    Add routes for homes, movies, movie details pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/details" element={<MovieDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 };
