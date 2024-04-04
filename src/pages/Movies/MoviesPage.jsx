859c104d9c3622613a6a3e7ccc6889d04ced5549
Teacher Radh <r.kamal@ph.goit.global>
2024-04-04 15:05:51 +0800

    Apply `useSearchParams` in MoviesPage

--- src/pages/Movies/MoviesPage.jsx
+++ src/pages/Movies/MoviesPage.jsx
@@ -1,23 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchMovieByQuery } from 'api/api';
 import { MovieList } from 'components/MovieList/MovieList';
 import { Outlet } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import css from './MoviesPage.module.css';
 
 export const MoviesPage = () => {
-  const [searchQuery, setSearchQuery] = useState('Friends');
+  // const [searchQuery, setSearchQuery] = useState('Friends');
   const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const movieName = searchParams.get('query') ?? 'Friends'; // Default search query is 'Friends'
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const updateQueryString = query => {
+    const nextParams = query !== '' ? { query } : {};
+    setSearchParams(nextParams);
+  };
 
   const fetchMovies = async () => {
-    if (!searchQuery.trim()) return;
+    if (!movieName.trim()) return;
+    setIsLoading(true);
+
     try {
-      const movies = await fetchMovieByQuery(searchQuery);
+      const movies = await fetchMovieByQuery(movieName);
       console.log('movies', movies);
       setMovies(movies);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
+      setIsLoading(false);
     }
   };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [movieName]);
 
   return (
     <div>
@@ -25,12 +44,17 @@ export const MoviesPage = () => {
         <input
           type="text"
           className={css.input}
-          onChange={e => setSearchQuery(e.target.value)}
+          onChange={e => updateQueryString(e.target.value)}
           placeholder="Search movies..."
         />
-        <button onClick={fetchMovies}>Search</button>
+        {/* Removed the button since the search updates automatically with the input */}
+        {/* <button onClick={fetchMovies}>Search</button> */}
       </div>
-      <MovieList movies={movies} />
+      {isLoading ? (
+        <p style={{ textAlign: 'center' }}>Loading...</p>
+      ) : (
+        <MovieList movies={movies} />
+      )}
       <Outlet />
     </div>
   );
