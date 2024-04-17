8c7b4593ce25f06f2b42a859d0f63ac4cb895971
Teacher Radh <r.kamal@ph.goit.global>
2024-04-17 18:14:43 +0800

    Add selectors for loading and error promise resolution

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,6 +2,7 @@
 
 // Selector for retrieving contacts from the state
 export const getContacts = state => state.contacts.contacts;
-
+export const getIsLoading = state => state.contacts.isLoading;
+export const getError = state => state.contacts.error;
 // Selector for retrieving the current filter value
 export const getFilter = state => state.filter;
