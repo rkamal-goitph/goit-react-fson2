3b7cdd2e2bba8c25b226864ad521bda5943ea84b
Radhwana Kamal <r.kamal@ph.goit.global>
2024-04-23 18:17:58 +0800

    Create Login and Register Pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,20 @@
 import { SharedLayout } from 'pages/SharedLayout';
 import { HomePage } from 'pages/HomePage';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
 
 export const App = () => {
   return (
     <>
-      <Router basename="/goit-react-hw-08-phonebook">
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<HomePage />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
     </>
   );
 };
