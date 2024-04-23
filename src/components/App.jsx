fa86b550c33ed148ab11fc7eda6777c83e9a071a
Radhwana Kamal <r.kamal@ph.goit.global>
2024-04-23 17:56:48 +0800

    Create SharedLayout and transfer Phonebook to HomePage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,18 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchContacts } from '../redux/contacts/contactOperations';
-import { setFilter } from '../redux/filter/filterSlice';
-import { addContact, deleteContact } from '../redux/contacts/contactOperations';
-import {
-  selectVisibleContacts,
-  selectIsLoading,
-  selectFilter,
-  selectError,
-} from '../redux/contacts/contactSelectors';
-import { ContactForm } from './ContactForm/ContactForm';
-import { Filter } from './Filter/Filter';
-import { ContactList } from './ContactList/ContactList';
+import { SharedLayout } from 'pages/SharedLayout';
+import { HomePage } from 'pages/HomePage';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
-  const visibleContacts = useSelector(selectVisibleContacts);
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
-  const filter = useSelector(selectFilter);
-
-  const dispatch = useDispatch();
-
-  console.log('visibleContacts', visibleContacts);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
-  const handleAddContact = newContact => {
-    // Placeholder for future Redux action
-    dispatch(addContact(newContact));
-  };
-
-  const handleDeleteContact = id => {
-    // Placeholder for future Redux action
-    dispatch(deleteContact(id));
-  };
-
-  const handleSetFilter = newFilter => {
-    // Placeholder for future Redux dispatch to update filter
-    dispatch(setFilter(newFilter));
-  };
-
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm addContact={handleAddContact} contacts={visibleContacts} />
-
-      <h2>Contacts</h2>
-      <Filter filter={filter} setFilter={handleSetFilter} />
-      {isLoading && (
-        <b style={{ display: 'block', padding: '0 0 20px 10px' }}>Loading...</b>
-      )}
-      {error && <b>Error: {error}</b>}
-      {visibleContacts && (
-        <ContactList
-          contacts={visibleContacts}
-          deleteContact={handleDeleteContact}
-        />
-      )}
-    </div>
+    <>
+      <Router basename="/goit-react-hw-08-phonebook">
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<HomePage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 };
---
+++ src/pages/SharedLayout.jsx
@@ -0,0 +1,10 @@
+import { Navigation } from 'components/Navigation/Navigation';
+import React from 'react';
+
+export const SharedLayout = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+};
