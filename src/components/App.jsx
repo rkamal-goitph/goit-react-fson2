2e744205b805743c15078dce7c566d6f8aff46ce
Radhwana Kamal <r.kamal@ph.goit.global>
2024-07-03 18:33:18 +0800

    Refactor movie details as nested route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,10 +16,11 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="movies" element={<MoviesPage />} />
-          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="cast" element={<CastList />} />
-            <Route path="reviews" element={<ReviewsList />} />
+          <Route path="movies" element={<MoviesPage />}>
+            <Route path=":movieId" element={<MovieDetailsPage />}>
+              <Route path="cast" element={<CastList />} />
+              <Route path="reviews" element={<ReviewsList />} />
+            </Route>
           </Route>
         </Route>
         <Route path="*" element={<NotFoundPage />} />
